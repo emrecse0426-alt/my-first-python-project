c0beebd9d9a81caf9461c68cd488fe834fa29a48
emrecse0426-alt <emrecse0426@gmail.com>
2025-11-13 16:43:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def add_numbers():
     )
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
-    
+    # Listen on all interfaces so Docker can access it
+    app.run(host='0.0.0.0', port=8000)
